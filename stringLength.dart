966c1f222358297dc7959dcfa23434006879471f
David Ochieng <159762944+davie2024@users.noreply.github.com>
2024-04-12 16:13:25 +0300

    Create stringLength.dart

---
+++ stringLength.dart
@@ -0,0 +1,9 @@
+int stringLength(String text) {
+  return text.length;
+}
+
+void main() {
+  String exampleString = "Hello, world!";
+  int length = stringLength(exampleString);
+  print("Length of the string: $length");
+}
